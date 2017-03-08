83140366248f6fc76ed3e4f3f4fd18bcfb6e9c96
min.tang <min.tang@ef.com>
2017-03-09 07:56:19 +0800

    simple excel

--- src/page/excel/index.ts
+++ src/page/excel/index.ts
@@ -1,47 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {data, headers} from "./model/Table";
+import {TextAreaCounter} from "./view/TextAreaCounter";
+import {Excel} from "./view/Excel";
 
-let TextAreaCounter = React.createClass({
-    propTypes: {
-        defaultValue: React.PropTypes.string
-    },
-    getDefaultProps: function () {
-        return {
-            text: ""
-        }
-    },
-    getInitialState: function () {
-        return {
-            text: this.props.defaultValue
-        }
-    },
-    componentWillReceiveProps: function (newProps) {
-        this.setState({
-            text: (<any>newProps).defaultValue
-        })
-    },
-    _textChange: function (e: React.FormEvent<HTMLTextAreaElement>) {
-        this.setState({
-            text: (<HTMLTextAreaElement>e.target).value
-        })
-    },
-    render: function () {
-        return React.DOM.div(null, 
-            React.DOM.textarea({
-                value: this.state.text,
-                onChange: this._textChange
-            }),
-            React.DOM.h3(null, this.state.text.length)
-        )
-    }
-})
+// var myTextAreaCounter = ReactDOM.render(
+//     React.createElement(TextAreaCounter, <any>{
+//         defaultValue: "tangkikodo"
+//     }), 
+//     document.getElementById("app")
+// )
 
-var myTextAreaCounter = ReactDOM.render(
-    React.createElement(TextAreaCounter, <any>{
-        defaultValue: "tangkikodo"
-    }), 
+ReactDOM.render(
+    React.createElement(Excel, {
+        headers: headers,
+        initialData: data,
+    }),
     document.getElementById("app")
 )
-
-setTimeout(() => {
-}, 1000);
