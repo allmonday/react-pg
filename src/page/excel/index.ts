87fd404d5600123c1ac9719b9bedd926b1306f67
min.tang <min.tang@ef.com>
2017-03-08 22:46:57 +0800

    state

--- src/page/excel/index.ts
+++ src/page/excel/index.ts
@@ -1,35 +1,47 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-let MyComponent = React.createClass({
+let TextAreaCounter = React.createClass({
     propTypes: {
-        name: React.PropTypes.string.isRequired
+        defaultValue: React.PropTypes.string
     },
     getDefaultProps: function () {
         return {
-            name: "tangkikodo"
+            text: ""
         }
     },
+    getInitialState: function () {
+        return {
+            text: this.props.defaultValue
+        }
+    },
+    componentWillReceiveProps: function (newProps) {
+        this.setState({
+            text: (<any>newProps).defaultValue
+        })
+    },
+    _textChange: function (e: React.FormEvent<HTMLTextAreaElement>) {
+        this.setState({
+            text: (<HTMLTextAreaElement>e.target).value
+        })
+    },
     render: function () {
-        return React.DOM.span(null, "my name is " + this.props.name)
+        return React.DOM.div(null, 
+            React.DOM.textarea({
+                value: this.state.text,
+                onChange: this._textChange
+            }),
+            React.DOM.h3(null, this.state.text.length)
+        )
     }
 })
 
-ReactDOM.render(
-    React.createElement(MyComponent, {
-        name: "tangkikodo"
+var myTextAreaCounter = ReactDOM.render(
+    React.createElement(TextAreaCounter, <any>{
+        defaultValue: "tangkikodo"
     }), 
     document.getElementById("app")
 )
 
-// ReactDOM.render(
-//     React.DOM.h1({
-//         id: "hello-world",
-//         style: {
-//             background: "black",
-//             color: "#fff",
-//             textAlign: "center"
-//         }
-//     }, "Hello world"),
-//     document.getElementById("app")
-// )
+setTimeout(() => {
+}, 1000);
