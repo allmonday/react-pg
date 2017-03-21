80167d4d48fec0475f1d70e248e257824dc5971c
tangkikodo <allmonday@126.com>
2017-03-21 14:40:32 +0800

    more test

--- src/page/api/index.ts
+++ src/page/api/index.ts
@@ -1,20 +1,77 @@
 import * as angular from "angular";
+import "angular-jwt";
+import "angular-animate";
+import "angular-aria";
+import "angular-material"
 
+angular.module("app", [ 'angular-jwt', 'ngMaterial'])
+    .config(function ($httpProvider: angular.IHttpProvider, jwtOptionsProvider: any) {
+        jwtOptionsProvider.config({
+            tokenGetter: function () {
+                let jwt = localStorage.getItem('jwt');
+                return jwt;
+            },
+            whiteListedDomains: ['localhost'],
+        });
 
-angular.module("app", [])
-    .controller("appController", function ($scope: angular.IScope, $http: angular.IHttpService) {
-        // $http.get("//localhost:3000/users").then(function (data) {
-        //     console.log(data);
-        // }, function (error) {
-        //     console.error(error);
-        // })
-        $http.post("//localhost:3000/authenticate", {
-            email: "kimiaj@126.com",
-            password: "123456"
-        }).then(function (data) {
-            console.log(data); 
-        }, function (error) {
-            console.error(error); 
-        })
+        $httpProvider.interceptors.push('jwtInterceptor');
+    })
+    .controller("appController", function (
+        $scope: angular.IScope, 
+        $http: angular.IHttpService, 
+        jwtHelper) {
+            let vm = this;
+            vm.model = {
+                name: "",
+                email: "kimiaj@126.com",
+                password: "123456",
+                posts: [],
+                payload: "",
 
+                login: () => {
+                    $http.post("//localhost:3000/authenticate", {
+                        email: vm.model.email,
+                        password: vm.model.password
+                    }).then(function (response) {
+                        console.log(response);
+                        let jwt = (<any>response.data).auth_token;
+                        localStorage.setItem('jwt', jwt);
+                        let expToken = jwtHelper.decodeToken(jwt);
+                        vm.model.payload = expToken;
+                    }, function (error) {
+                        console.error(error); 
+                    })
+                },
+                register: () => {
+                    $http.post("//localhost:3000/users", {
+                        email: vm.model.email,
+                        password: vm.model.password
+                    }).then(function (response) {
+                    }, function (error) {
+                        console.error(error); 
+                    })
+                },
+                refresh: () => {
+                    $http({
+                        method: 'get',
+                        url: "//localhost:3000/posts"
+                    }).then(function(response) {
+                        vm.model.posts = response.data;
+                    }, function () {})
+                },
+                title: "",
+                body: "",
+                addPost: () => {
+                    $http({
+                        method: "post",
+                        url: "//localhost:3000/posts",
+                        data: {
+                            title: vm.model.title, 
+                            body: vm.model.body
+                        }
+                    }).then(function (response) {
+                        vm.model.refresh();
+                    })
+                }
+            }
     })
